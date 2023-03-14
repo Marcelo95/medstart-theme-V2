145db65dfd53616dc334e36b4e7b5075d1d082f7
Marcelo Vieira <marcelovieira1995@gmail.com>
2023-03-14 00:58:35 -0300

    correcao css

--- tema/templates/components/popup/content.php
+++ tema/templates/components/popup/content.php
@@ -1,12 +1,12 @@
 
 <?php if( $args["first_run"] ): ?>
-    <link rel="stylesheet" href="<?php _e(asset(sprintf("../templates/components/%s/style.css", basename(__DIR__)))); ?>">
+    <link rel="stylesheet" href="<?php _e(asset(sprintf("../templates/components/%s/style.css?v1", basename(__DIR__)))); ?>">
 
 <?php endif; ?>
 
 <!-- Modal -->
 <div class="modal fade myPopup" id="<?php _e($args["id"]  ?? 'teste'); ?>" tabindex="-1" role="dialog" aria-labelledby="<?php _e($args["id"]  ?? 'teste'); ?>Label" aria-hidden="true">
-    <div class="modal-dialog  modal-dialog-centered modal-xl" role="document">
+    <div class="modal-dialog  modal-dialog-centered  <?php _e($args["css_modal"]  ?? 'modal-xl'); ?>" role="document">
         <div class="modal-content ">
             <div class="modal-header">
                 <h5 class="modal-title" id="<?php _e($args["id"]  ?? 'teste'); ?>Label"><?php _e($args["title"]  ?? ''); ?></h5>
